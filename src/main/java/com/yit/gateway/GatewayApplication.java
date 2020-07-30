212ff4c7f0717df96dc219dc1d9266bc6ac0a789
Kriangsak Pornpantiva <kriangsak.po@yipintsoi.com>
2020-07-30 15:36:39 +0700

    using spring cloud gateway

--- src/main/java/com/yit/gateway/GatewayApplication.java
+++ src/main/java/com/yit/gateway/GatewayApplication.java
@@ -2,6 +2,9 @@ package com.yit.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayApplication {
@@ -10,4 +13,20 @@ public class GatewayApplication {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
 
+	@Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+		
+		return builder.routes()
+			      .route(r ->r
+			        .path("/getProduct")
+			        .uri("http://catalog-git:8080")
+			        .id("Product"))
+			      .route(r -> r
+			        .path("/getCart")
+			        .filters(f -> f.prefixPath("/api"))
+			        .uri("http://cart-git:8080")
+			        .id("Cart"))
+			    .build();
+		
+    }
 }
