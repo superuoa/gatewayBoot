bd00c9ebf52f9ba4d8ad17df7cd797612e75a192
Kriangsak Pornpantiva <kriangsak.po@yipintsoi.com>
2020-08-18 11:13:49 +0700

    update name container

--- src/main/java/com/yit/gateway/GatewayApplication.java
+++ src/main/java/com/yit/gateway/GatewayApplication.java
@@ -19,22 +19,22 @@ public class GatewayApplication {
 		return builder.routes()
 			      .route(r ->r
 			    		  .path("/getProduct")
-			    		  .uri("http://catalog-git:8080")
+			    		  .uri("http://catalog:8080")
 			    		  .id("Product"))
 			      .route(r -> r
 			    		  .path("/getCart")
 			    		  .filters(f -> f.prefixPath("/api"))
-			    		  .uri("http://cart-git:8080")
+			    		  .uri("http://cart:8080")
 			    		  .id("Cart"))
 			      .route(r -> r
 			    		  .path("/addToCart")
 			    		  .filters(f -> f.prefixPath("/api"))
-			    		  .uri("http://cart-git:8080")
+			    		  .uri("http://cart:8080")
 			    		  .id("Cart"))
 			      .route(r -> r
 			    		  .path("/deleteItem")
 			    		  .filters(f -> f.prefixPath("/api"))
-			    		  .uri("http://cart-git:8080")
+			    		  .uri("http://cart:8080")
 			    		  .id("Cart"))
 			    .build();
 	
